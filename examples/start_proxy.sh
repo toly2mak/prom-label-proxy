39e078d46cafae15d6f1f86ef0b248274f875328
Toly Makarov <toly.makarov@qdrant.com>
2024-10-16 23:46:52 -0400

    update examples

--- examples/start_proxy.sh
+++ examples/start_proxy.sh
@@ -10,5 +10,4 @@ LOGLEVEL=DEBUG ./prom-label-proxy \
    -result-fstring 'qdrant-%s.+' \
    -upstream https://prometheus-monitoring.eu-central-1-0.aws.staging-cloud.qdrant.io \
    -rewrite-metrics-path '/sys_metrics' \
-   -only-metrics=false \
    -insecure-listen-address 127.0.0.1:8080
